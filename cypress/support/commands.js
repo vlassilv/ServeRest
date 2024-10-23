e0c099fb869d8740d67d74ad529bacef7bb4aedd
vlassilv <valmirsystem@gmail.com>
2024-10-23 14:26:01 -0300

    - Implementation of the RegisterUser.spec.cy.js test file to validate the user registration functionality; - Creation of customized commands to generate random names, emails and passwords. - Implementation of the RegisterPage class code;

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,21 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Função para gerar um nome aleatório
+Cypress.Commands.add('generateRandomName', () => {
+  const names = ['John', 'Jane', 'Alex', 'Chris', 'Pat']; // Exemplos de nomes
+  const randomIndex = Math.floor(Math.random() * names.length);
+  return `${names[randomIndex]}-${Date.now()}`;
+});
+
+// Função para gerar um email aleatório
+Cypress.Commands.add('generateRandomEmail', () => {
+  return `user${Date.now()}@test.com`;
+});
+
+// Função para gerar uma senha aleatória
+Cypress.Commands.add('generateRandomPassword', () => {
+  const randomString = Math.random().toString(36).substring(2, 10); // Gera uma string aleatória
+  return `Senha${randomString}!`; // Cria uma senha segura
+});
---
+++ cypress/support/pages/RegisterPage.js
@@ -0,0 +1,36 @@
+class RegisterPage{
+    visit(){
+        // Visitar a página de registro
+        cy.visit('https://front.serverest.dev/cadastrarusuarios');
+    }
+
+    fillName(nameUser){
+        // Preencher o campo de nome
+        cy.get('#nome').type(nameUser);
+    }
+
+    fillEmail(email){
+        // Preencher o campo de e-mail
+        cy.get('#email').type(email);
+    }
+  
+    fillPassword(password){
+        // Preencher o campo de senha
+        cy.get('#password').type(password);
+    }
+  
+    submit(){
+        // Clicar no botão de registro
+        cy.get('[data-testid="cadastrar"]').click();
+    }
+  
+    verifySuccess(){
+        // Verifica se o registro foi bem-sucedido (exemplo: uma mensagem de sucesso)
+        cy.contains('Cadastro realizado com sucesso').should('be.visible');
+    }
+
+}
+
+  
+export default new RegisterPage();
+  
